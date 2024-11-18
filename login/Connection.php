d3707cd27b170b58ff63f99aef6ae4383b0c50d6
liamsimfty <muftiirsyal@gmail.com>
2024-11-18 17:33:10 +0700

    rapi rapi

--- login/Connection.php
+++ login/Connection.php
@@ -1,9 +1,7 @@
 <?php
-$conn = oci_connect('system', '', 'localhost/xe');
+$conn = oci_connect('system', 'mufti123', 'localhost/xe');
 if (!$conn) {
     $e = oci_error();
     trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
 }
-
-
 ?>
