6c7b09b49b497a9262d8253a6a55e247bb52e6e7
4lfrzzi <slthnia.04@gmail.com>
2024-12-22 17:13:24 +0700

    sip

---
+++ features/pages/profile.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"/>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <title>ORON Profile</title>
+    <link rel="stylesheet" href="../../Styling/css/profile.css">
+</head>
+<body>
+    <!-- Navbar Start -->
+    <nav class="navbar">
+        <div class="navbar-logo">
+            <img src="../../Styling/images/oron-logo.png" class="navbar-logo-img" alt="logo"/>
+            <h1>ORON</h1>
+        </div>
+        <div class="navbar-link">
+            <ul class="navbar-list">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Store</a></li>
+                <li><a href="#">Blog</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#"><i class="fa-solid fa-magnifying-glass"></i></a></li>
+                <li><a href="#"><i class="fa-solid fa-cart-shopping"></i></a></li>
+            </ul>
+        </div>
+    </nav>    
+    <!-- Navbar Start -->
+
+    <!-- Profile -->
+    <main>
+        <h1><span>Edit</span> Profile</h1>
+        <div class="container">
+            <img src="images/user.jpg" alt="Profile Picture">
+            <div class="profile-info">
+                <div>
+                    <span>Nama</span>
+                    <span>user</span>
+                </div>
+                <div>
+                    <span>Username</span>
+                    <span>U53R</span>
+                </div>
+                <div>
+                    <span>E-mail</span>
+                    <span>user@pnj.com</span>
+                </div>
+                <div>
+                    <span>Birthday</span>
+                    <span>21-12-2024</span>
+                </div>
+            </div>
+            <div class="edit-button">
+                <button>Edit</button>
+            </div>
+        </div>
+    </main>
+    <!-- Profile -->
+</body>
+</html>
