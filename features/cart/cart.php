44063e58a2e0979560d597b117dc0a0b8b1a5222
kefas hutabarat <kefaskefas10@gmail.com>
2024-12-23 06:08:59 +0700

    tambahan aja

--- features/cart/cart.php
+++ features/cart/cart.php
@@ -171,56 +171,56 @@ oci_close($conn);
 
 </head>
 <body>
-<header>
-    <div>
-        <nav class="navbar">
-            <img src="../../Styling/images/oron-logo.png" class="navbar-logo" alt="logo" />
-            <h1>ORON</h1>
-            <ul class="navbar-list">
-            <li><a href="../pages/homepage.php">Home</a></li>
-            <li><a href="../store/store.php">Store</a></li>
-            <li><a href="../pages/blog.php">Library</a></li>
-            <li><a href="../pages/about.php">About</a></li>
-            <li><a href="../cart/cart.php"><i class="fa-solid fa-cart-shopping"></i></a></li>
-            </ul>
-        
-            <div class="profile-dropdown">
-            <div onclick="toggle()" class="profile-dropdown-btn">
-                <div class="profile-img">
-                <i class="fa-solid fa-circle"></i>
-                </div>
-                
-                <span>
-                <?php if ($isLoggedIn): ?>
-                    <?php echo htmlspecialchars($_SESSION['username']); ?>
-                <?php else: ?>
-                    Guest
-                <?php endif; ?>
-                <i class="fa-solid fa-angle-down"></i>
-                </span>
-            </div>
-        
-            <ul class="profile-dropdown-list">
-                <?php if ($isLoggedIn): ?>
-                <li class="profile-dropdown-list-item">
-                    <a href="../login/login.php">
-                    <i class="fa-solid fa-arrow-right-from-bracket"></i>
-                    Log out
-                    </a>
-                </li>
-                <?php else: ?>
-                <li class="profile-dropdown-list-item">
-                    <a href="../login/login.php">
-                    <i class="fa-solid fa-arrow-right-from-bracket"></i>
-                    Log in
-                    </a>
-                </li>
-                <?php endif; ?>
-            </ul>
-            </div>
-        </nav>
-        </div>
-</header>
+<nav class="navbar">
+  <div class="navbar-logo">
+    <img src="../../Styling/images/oron-logo.png" class="navbar-logo" alt="logo" />
+    <h1>ORON</h1>
+  </div>
+  <div class="navbar-link">
+  <ul class="navbar-list">
+    <li><a href="../pages/homepage.php">Home</a></li>
+    <li><a href="../store/store.php">Store</a></li>
+    <li><a href="../library/library.php">library</a></li>
+    <li><a href="../pages/about.php">About</a></li>
+    <li><a href="../cart/cart.php"><i class="fa-solid fa-cart-shopping"></i></a></li>
+  </ul>
+
+  <div class="profile-dropdown">
+    <div onclick="toggle()" class="profile-dropdown-btn">
+      <div class="profile-img">
+        <i class="fa-solid fa-circle"></i>
+      </div>
+      
+      <span>
+        <?php if ($isLoggedIn): ?>
+          <?php echo htmlspecialchars($_SESSION['username']); ?>
+        <?php else: ?>
+          Guest
+        <?php endif; ?>
+        <i class="fa-solid fa-angle-down"></i>
+      </span>
+    </div>
+
+    <ul class="profile-dropdown-list">
+      <?php if ($isLoggedIn): ?>
+        <li class="profile-dropdown-list-item">
+          <a href="../login/login.php">
+            <i class="fa-solid fa-arrow-right-from-bracket"></i>
+            Log out
+          </a>
+        </li>
+      <?php else: ?>
+        <li class="profile-dropdown-list-item">
+          <a href="../login/login.php">
+            <i class="fa-solid fa-arrow-right-from-bracket"></i>
+            Log in
+          </a>
+        </li>
+      <?php endif; ?>
+    </ul>
+  </div>
+  </div>
+</nav>
 <div class="cart">
     <h1>Cart</h1>
         <form method="POST" class="cart-container">
